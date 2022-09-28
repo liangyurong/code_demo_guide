e9efa2dce1d9cb89ffe65c3a4730b9f5bbbfc37d
liangyurong <794842744@qq.com>
2022-09-28 21:56:13 +0800

    2022-09-28 excel转pdf去水印

--- src/main/java/com/demo/lyr/tool/pdf/ChargePdfUtils.java
+++ src/main/java/com/demo/lyr/tool/pdf/ChargePdfUtils.java
@@ -1,30 +1,30 @@
-package com.demo.lyr.tool.pdf;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import com.spire.xls.FileFormat;
-import com.spire.xls.Workbook;
-/**
- * 收费的pdf工具，不建议使用，有水印
- * @author liangyurong
- * @date 2022-09-21 19:31
- */
-@Api(tags = "PDF工具")
-public class ChargePdfUtils {
-
-    public static void main(String[] args) {
-        ChargePdfUtils.excelToPdf();
-    }
-
-    @ApiOperation(value = "excel转pdf", notes = "excel转pdf")
-    public static void excelToPdf(){
-        //创建一个Workbook实例并加载Excel文件
-        Workbook workbook = new Workbook();
-        workbook.loadFromFile("C:\\Users\\yelin\\Desktop\\批生产指令.xlsm");
-        //设置转换后的PDF页面高宽适应工作表的内容大小
-        workbook.getConverterSetting().setSheetFitToPage(true);
-        //将生成的文档保存到指定路径
-        workbook.saveToFile("C:\\Users\\yelin\\Desktop\\demo.pdf", FileFormat.PDF);
-    }
-
-}
+//package com.demo.lyr.tool.pdf;
+//
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import com.spire.xls.FileFormat;
+//import com.spire.xls.Workbook;
+///**
+// * 收费的pdf工具，不建议使用，有水印
+// * @author liangyurong
+// * @date 2022-09-21 19:31
+// */
+//@Api(tags = "PDF工具")
+//public class ChargePdfUtils {
+//
+//    public static void main(String[] args) {
+//        ChargePdfUtils.excelToPdf();
+//    }
+//
+//    @ApiOperation(value = "excel转pdf", notes = "excel转pdf")
+//    public static void excelToPdf(){
+//        //创建一个Workbook实例并加载Excel文件
+//        Workbook workbook = new Workbook();
+//        workbook.loadFromFile("C:\\Users\\yelin\\Desktop\\批生产指令.xlsm");
+//        //设置转换后的PDF页面高宽适应工作表的内容大小
+//        workbook.getConverterSetting().setSheetFitToPage(true);
+//        //将生成的文档保存到指定路径
+//        workbook.saveToFile("C:\\Users\\yelin\\Desktop\\demo.pdf", FileFormat.PDF);
+//    }
+//
+//}
---
+++ src/main/java/com/demo/lyr/tool/pdf/PdfUtils3.java
@@ -0,0 +1,78 @@
+package com.demo.lyr.tool.pdf;
+
+import com.aspose.cells.License;
+import com.aspose.cells.PdfSaveOptions;
+import com.aspose.cells.Workbook;
+import sun.misc.BASE64Decoder;
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+public class PdfUtils3 {
+
+    public static void main(String[] args){
+        String sourceFilePath = "C:\\Users\\yurong333\\Desktop\\包装指令.xlsm";
+        String desFilePath = "C:\\Users\\yurong333\\Desktop\\aa.pdf";
+        excel2pdf(sourceFilePath, desFilePath);
+    }
+
+    /**
+     * 获取license 去除水印
+     *
+     * @return
+     */
+    public static boolean getLicense() {
+        try {
+            String license = "PExpY2Vuc2U+DQogIDxEYXRhPg0KICAgIDxQcm9kdWN0cz4NCiAgICAgIDxQcm9kdWN0PkFzcG9zZS5Ub3RhbCBmb3IgSmF2YTwvUHJvZHVjdD4NCiAgICAgIDxQcm9kdWN0PkFzcG9zZS5Xb3JkcyBmb3IgSmF2YTwvUHJvZHVjdD4NCiAgICA8L1Byb2R1Y3RzPg0KICAgIDxFZGl0aW9uVHlwZT5FbnRlcnByaXNlPC9FZGl0aW9uVHlwZT4NCiAgICA8U3Vic2NyaXB0aW9uRXhwaXJ5PjIwOTkxMjMxPC9TdWJzY3JpcHRpb25FeHBpcnk+DQogICAgPExpY2Vuc2VFeHBpcnk+MjA5OTEyMzE8L0xpY2Vuc2VFeHBpcnk+DQogICAgPFNlcmlhbE51bWJlcj44YmZlMTk4Yy03ZjBjLTRlZjgtOGZmMC1hY2MzMjM3YmYwZDc8L1NlcmlhbE51bWJlcj4NCiAgPC9EYXRhPg0KICA8U2lnbmF0dXJlPnNOTExLR01VZEYwcjhPMWtLaWxXQUdkZ2ZzMkJ2SmIvMlhwOHA1aXVEVmZaWG1ocHBvK2QwUmFuMVA5VEtkalY0QUJ3QWdLWHhKM2pjUVRxRS8ySVJmcXduUGY4aXROOGFGWmxWM1RKUFllRDN5V0U3SVQ1NUd6NkVpalVwQzdhS2Vvb2hUYjR3MmZwb3g1OHdXb0YzU05wNnNLNmpEZmlBVUdFSFlKOXBqVT08L1NpZ25hdHVyZT4NCjwvTGljZW5zZT4=";
+            InputStream is = BaseToInputStream(license);
+            License aposeLic = new License();
+            aposeLic.setLicense(is);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * base64转inputStream
+     *
+     * @param base64string
+     * @return
+     */
+    private static InputStream BaseToInputStream(String base64string) {
+        ByteArrayInputStream stream = null;
+        try {
+            BASE64Decoder decoder = new BASE64Decoder();
+            byte[] bytes1 = decoder.decodeBuffer(base64string);
+            stream = new ByteArrayInputStream(bytes1);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return stream;
+    }
+
+    /**
+     * excel 转为pdf 输出。
+     *
+     * @param sourceFilePath excel文件
+     * @param desFilePathd   pad 输出文件目录
+     */
+    public static void excel2pdf(String sourceFilePath, String desFilePathd) {
+        // 验证License 若不验证则转化出的pdf文档会有水印产生
+        if (!getLicense()) {
+            return;
+        }
+        try {
+            Workbook wb = new Workbook(sourceFilePath);// 原始excel路径
+            FileOutputStream fileOS = new FileOutputStream(desFilePathd);
+            PdfSaveOptions pdfSaveOptions = new PdfSaveOptions();
+            pdfSaveOptions.setOnePagePerSheet(true);//把内容放在一张PDF 页面上；
+            wb.save(fileOS, pdfSaveOptions);
+            fileOS.flush();
+            fileOS.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
